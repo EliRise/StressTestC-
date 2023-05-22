42b4fac1f535340bf4f8fa3b4b3f1540ecc981ed
EliRise <81958264+EliRise@users.noreply.github.com>
2023-05-22 22:26:26 +0300

    Delete 1/build-Test-Desktop_Qt_5_15_2_MinGW_32_bit-Debug directory

--- 1/build-Test-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/Test_autogen/EWIEGA46WW/moc_TableRow.cpp.d
+++
@@ -1,2 +0,0 @@
-C:/StressTestC++/1/build-Test-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/Test_autogen/EWIEGA46WW/moc_TableRow.cpp: C:/StressTestC++/project/Test/TableRow.h \
-  C:/StressTestC++/1/build-Test-Desktop_Qt_5_15_2_MinGW_32_bit-Debug/Test_autogen/moc_predefs.h
